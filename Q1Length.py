8011aa628859424857db199ef28ed9e501db1a3a
Aiden Vu <100857445+AidenVu1@users.noreply.github.com>
2023-05-08 13:51:38 +0000

    finished

--- Q1Length.py
+++ Q1Length.py
@@ -1,2 +1,6 @@
-# Create a program that will accept a word from a user and return the length of that word. 
-# Make this program in a loop with option to exit when the use types in quit.
+while True:
+    word = input("Enter a word: ")
+    if word == "quit":
+        break
+    print("The length of the word is:", len(word))
+
--- Q3WordTriangle.py
+++ Q3WordTriangle.py
@@ -1,7 +1,4 @@
-# Create a program that will prompt the user for a word, and return a 'word triangle'. For example, if the user types in the word "PYTHON", your program will output:
-# P
-# PY
-# PYT
-# PYTH
-# PYTHO
-# PYTHON
+word = input("Enter a word: ")
+for i in range(len(word)):
+    print(word[:i+1])
+
